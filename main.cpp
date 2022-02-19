db199f0e54c9e758907d1eca7720089e62e64cdd
Ved9rakash <vs19990711@gmail.com>
2022-02-19 17:54:45 +0530

    vec3 addition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include "vec3.hpp"
+#include "color.hpp"
 
+#include <iostream>
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	const int image_width = 256;
+	const int image_height = 256;
+
+	//Render
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+
+	for (int j = image_height - 1; j >=0; --j) {
+		std::cerr << "\rScanLines Remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) {
+			color3 pixel_color(
+					double(i)/(image_width - 1),
+					double(j)/(image_height - 1),
+					0.25
+					);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::cerr << "\nDone.\n";
 }
