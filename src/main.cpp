8d624b7fd8275adf8542e3a9c1d99ba7d427fae3
Ved9rakash <vs19990711@gmail.com>
2022-02-20 13:39:33 +0530

    ray tracer

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,33 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "vec3.hpp"
 #include "color.hpp"
+#include "ray.hpp"
+#include "imgCam.hpp"
 
 #include <iostream>
 
 int main()
 {
-	const int image_width = 256;
-	const int image_height = 256;
 
-	//Render
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	//Rendering
+	std::cout << "P3\n" << image::image_width << ' ' << image::image_height << "\n255\n";
 
 
-	for (int j = image_height - 1; j >=0; --j) {
+	for (int j = image::image_height - 1; j >=0; --j) {
 		std::cerr << "\rScanLines Remaining: " << j << ' ' << std::flush;
-		for (int i = 0; i < image_width; ++i) {
-			color3 pixel_color(
-					double(i)/(image_width - 1),
-					double(j)/(image_height - 1),
-					0.25
+		for (int i = 0; i < image::image_width; ++i) {
+			auto u = double(i) / (image::image_width - 1);
+			auto v = double(i) / (image::image_height - 1);
+
+			ray r(
+					camera::origin,
+					camera::lower_left_corner + u*camera::horizontal - v*camera::vertical - camera::origin
 					);
-			write_color(std::cout, pixel_color);
+
+			color3 pixel_color = color::ray_color(r);
+			color::write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nDone.\n";
