8e92c01acfe836f624babb00bb5d084f48d5bd05
Ved9rakash <vs19990711@gmail.com>
2022-02-22 18:15:09 +0530

    ray mechanics

--- include/ray.hpp
+++ include/ray.hpp
@@ -9,16 +9,17 @@
 
 class ray {
 public:
-	ray() {};
-	ray(const point3& origin, const vec3& direction)
+	ray() = default;
+
+	[[maybe_unused]] ray(const point3& origin, const vec3& direction)
 		: orig(origin), dir(direction)
 	{}
 
-	point3 origin() const { return orig; }
-	vec3 direction() const { return dir;}
+	[[nodiscard]] point3 origin() const { return orig; }
+	[[nodiscard]] vec3 direction() const { return dir;}
 
-	point3 at(double scale) const {
-		return orig + scale * dir;
+	[[maybe_unused]] [[nodiscard]] point3 at(double scale) const {
+		return { orig + scale * dir };
 	}
 
 public:
